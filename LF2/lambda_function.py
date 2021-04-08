998ebd22868f2b273a35e51dbb9baa767190f9d7
Hariharan Jayakumar <46619584+hariharan-jayakumar@users.noreply.github.com>
2021-04-08 10:25:15 -0400

    Update lambda_function.py

--- LF2/lambda_function.py
+++ LF2/lambda_function.py
@@ -1,7 +1,13 @@
 import json
 import requests
 import boto3
+import inflect
 from requests_aws4auth import AWS4Auth
+
+
+def remove_plural(word):
+    check = inflect.engine().singular_noun(word)
+    return word if check is False else check
 
 def search_elastic_search(es_queries):
     region = 'us-east-1'  # For example, us-west-1
@@ -17,7 +23,8 @@ def search_elastic_search(es_queries):
             }
         }
     }
-    url = 'https://search-photos3-wrhauukzuqwxqu4vuq7bixrdda.us-east-1.es.amazonaws.com/photos3/_search/'
+    
+    url = 'https://search-photos-es4ieoq6di4xg3e2xhu5mcbumq.us-east-1.es.amazonaws.com/photos/_search/'
 
     r = requests.get(url, auth=("hariharanjaya", "Harirockz1!"), headers=headers, data=json.dumps(query))
     
@@ -29,27 +36,36 @@ def search_elastic_search(es_queries):
 
 def get_search_query(intent_request):
     slots = intent_request['slots']
-    search_items = ""
+    search_items = []
     for key, value in slots.items():
         if value is not None:
+            value = remove_plural(value)
             print(value)
-            search_items += value + " "
+            search_items.append(value)
+    
+    search_items = " OR ".join(search_items)
     
     print("Search queries", search_items)
     
     return search_items
 
 def lambda_handler(event, context):
-    print(event)
+    print("Event - ", event)
     
     client = boto3.client('lex-runtime')
+    
+    dummy_string = "show me pictures of amazons"
+    
+    dummy_string = event['queryStringParameters']['query']
+    print(dummy_string)
     
     # 'queryStringParameters': {'query': 'red'}
     response = client.post_text(
         botName='PhotoSearch',
         botAlias='photo_lex',
         userId='User1',
-        inputText=event['queryStringParameters']['query']
+        # inputText=event['queryStringParameters']['query']
+        inputText = dummy_string
     )
     
     print(response)
@@ -58,17 +74,26 @@ def lambda_handler(event, context):
     response_es = search_elastic_search(search_query)
     
     print(response_es)
+    
+    # prepare response
+    response = {}
+    response["results"] = []
+    for result in response_es['hits']['hits']:
+        response_object = {}
+        s3_url = 'https://photos-bucket-assignment2.s3.amazonaws.com/' + result["_source"]["objectKey"]
+        response_object["url"] = s3_url
+        response_object["labels"] = result["_source"]["labels"]
+        response["results"].append(response_object)
 
-    results = []
-    for hit in response_es['hits']['hits']:
-        result = 'https://photos-bucket-assignment2.s3.amazonaws.com/' + hit["_source"]["objectKey"]
-        results.append(result)
-
-    print(results)
     response_to_return = {
         'statusCode': 200,
-        'headers': {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
-        'body': list(set(results))
+        'headers': {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        'body': json.dumps(response)
     }
+    
     print(response_to_return)
     return response_to_return
